86402ce82c5a9f4485622f7ce8879c086c57326a
Matao Designs <matt@mataodesigns.com>
2018-08-03 01:18:50 -0400

    updating the work page

--- dist/js/main.js
+++ dist/js/main.js
@@ -31,6 +31,56 @@ function toggleMenu() {
   }
 }
 
+//Open project modal
 $(".view_project").click(function() {
-  let id = $(this).attr("id");
+  let name = $(this).attr("data");
+  $(".projectName").html(name);
+  var details = getProjectDetails(name);
+  var tech = details[1];
+  var techList = "";
+  for (let i = 0; i < tech.length; i++) {
+    techList += "<li>" + tech[i] + "</li>";
+  }
+
+  $(".modal-body #leftModal").html(
+    "<h2>Background</h2><p>" + details[0] + "</p>"
+  );
+  $(".modal-body #rightModal").html(
+    "<h2>Technologies/Languages Used</h2><ul>" + techList + "</ul>"
+  );
+  $(".modal_bg").show();
+  $(".modal").show("slow");
 });
+
+$(".close_modal").click(function() {
+  $(".modal_bg").hide();
+  $(".modal").hide("fast");
+});
+
+$(".modal_bg").click(function() {
+  $(".modal_bg").hide();
+  $(".modal").hide("fast");
+});
+
+function getProjectDetails(name) {
+  var details = new Array();
+
+  switch (name) {
+    case "Nationwide Construction Group":
+      details[0] =
+        "The Nationwide Construction Group website was designed and developed to provide detailed information on the products and services they offer to the general public. While the target demographic is residential customers, the site was also developed for commercial users to interact with the site by means of vendor portals.";
+      details[1] = new Array(
+        "HTML5",
+        "SCSS",
+        "CSS",
+        "Javascript",
+        "JQuery",
+        "PHP",
+        "MySQL",
+        "Bootstrap"
+      );
+      break;
+  }
+
+  return details;
+}
